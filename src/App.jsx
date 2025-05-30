2eb656140044a976882dce5babf98e5d9429530a
HKKavindaPathum <kavindapathum98@gmail.com>
2025-05-31 00:56:55 +0530

    add axuos & react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import HomePage from './pages/home'
 import LoginPage from './pages/login'
 import SignUpPage from './pages/signup'
 import AdminPage from './pages/admin'
+import TestPage from './pages/test'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
  
@@ -12,11 +14,13 @@ function App() {
   return (
     <BrowserRouter>
       <div >
+        <Toaster position='top-right'/>
         {/* <Header/> */}
         <Routes path="/*">
           <Route path='/' element={<HomePage/>}/> 
           <Route path='/login' element={<LoginPage/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/testing" element={<TestPage/>}/>
           <Route path='/admin/*' element={<AdminPage/>}/>
           <Route path='/*' element={<h1>404 Not Found</h1>}/>
         </Routes>
