d1aed25e827495ac04d1d95f80e47b713855c618
HKKavindaPathum <kavindapathum98@gmail.com>
2025-05-30 14:01:50 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/header'
+//import Header from './components/header'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import SignUpPage from './pages/signup'
+import AdminPage from './pages/admin'
 
 function App() {
+ 
 
   return (
-    <>
-      <h1>Hello</h1>
-      <h2>World</h2>
-      <Header/>
-    </>
+    <BrowserRouter>
+      <div >
+        {/* <Header/> */}
+        <Routes path="/*">
+          <Route path='/' element={<HomePage/>}/> 
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path='/admin/*' element={<AdminPage/>}/>
+          <Route path='/*' element={<h1>404 Not Found</h1>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
