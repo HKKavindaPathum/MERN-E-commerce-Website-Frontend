31f265446f6e9e858223078bb6a1ec3deebb5eac
HKKavindaPathum <kavindapathum98@gmail.com>
2025-06-07 14:46:34 +0530

    modify test

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,52 +1,32 @@
-import { useState } from "react";
+import { useState } from "react"
+import mediaUpload from "../utils/mediaUpload"
 
 export default function TestPage(){
 
-    const [count,setCount] = useState(0)
-    const [status,setStatus] = useState("Passed")
+    const [image, setImage] = useState(null)
+
+
+    function fileUpload(){
+        
+        mediaUpload(image).then(
+            (res)=>{
+                console.log(res)
+            }
+        ).catch(
+            (res)=>{
+                console.log(res)
+            }
+        )
+        //const url = await mediaUpload(image)
+    }
 
     return(
         <div className="w-full h-screen flex justify-center items-center  flex-col">
-            <div className="w-[450px] h-[250px] shadow flex justify-center items-center">
-                <button onClick={
-                    ()=>{
-                        setCount(count-1);
-                    }
-                } className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">
-                    -
-                </button>
-                <span className="text-[40px] font-bold text-center w-[100px] h-[40px] mx-[10px] flex justify-center items-center">
-                    {count}
-                </span>
-                <button onClick={
-                    ()=>{
-                        setCount(count+1)
-                    }
-                } className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">
-                    +
-                </button>
-            </div>
-            <div className="w-[450px] h-[250px] shadow flex justify-center items-center flex-col">
-                <span className="text-[40px] font-bold text-center w-[100px] h-[40px] mx-[10px] flex justify-center items-center">
-                    {status}
-                </span>
-                <div>
-                    <button onClick={
-                        ()=>{
-                            setStatus("Passed");
-                        }
-                    } className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer m-[20px]">
-                        Passed
-                    </button>
-                    <button onClick={
-                        ()=>{
-                            setStatus("Failed")
-                        }
-                    } className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer m-[20px]">
-                        Failed
-                    </button>
-                </div>
-            </div>
+            <input type="file" className="file-input file-input-bordered w-full max-w-xs"
+            onChange={(e)=>{
+                setImage(e.target.files[0])
+            }} />
+            <button onClick={fileUpload} className="bg-green-500 text-white font-bold py-2 px-4 rounded">Upload</button>  
         </div>
     )
 }
