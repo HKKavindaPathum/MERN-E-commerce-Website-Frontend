86f701e02b3a9dd727ce44d00387a1e91519ef8b
HKKavindaPathum <kavindapathum98@gmail.com>
2025-06-08 18:51:32 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 //import Header from './components/header'
 import HomePage from './pages/home'
 import LoginPage from './pages/login'
-import AdminPage from './pages/admin'
+import AdminPage from './pages/adminPage'
 import TestPage from './pages/test'
 import { Toaster } from 'react-hot-toast'
 import RegisterPage from './pages/register'
@@ -17,12 +17,11 @@ function App() {
         <Toaster position='top-right'/>
         {/* <Header/> */}
         <Routes path="/*">
-          <Route path='/' element={<HomePage/>}/> 
           <Route path='/login' element={<LoginPage/>}/>
           <Route path="/signup" element={<RegisterPage/>}/>
           <Route path="/testing" element={<TestPage/>}/>
           <Route path='/admin/*' element={<AdminPage/>}/>
-          <Route path='/*' element={<h1>404 Not Found</h1>}/>
+          <Route path='/*' element={<HomePage/>}/>
         </Routes>
       </div>
     </BrowserRouter>
