1375a0ab1f765383894a3543a0f9775a437965ab
HKKavindaPathum <kavindapathum98@gmail.com>
2025-07-10 00:07:38 +0530

    add Search Product page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,7 @@ import ProductPage from "./client/productPage";
 import ProductOverviewPage from "./client/productOverview";
 import CartPage from "./client/cart";
 import CheckoutPage from "./client/checkOut";
+import SearchProductPage from "./client/searchProducts";
 
 export default function HomePage(){
     return(
@@ -17,6 +18,7 @@ export default function HomePage(){
                     <Route path="/contact" element={<h1>Contact</h1>}/>
                     <Route path="/cart" element={<CartPage/>}/>
                     <Route path="/checkout" element={<CheckoutPage/>}/>
+                    <Route path="/search" element={<SearchProductPage />} />
                     <Route path="/overview/:id" element={<ProductOverviewPage/>}/>
                     <Route path="/*" element={<h1>404 Not Found</h1>}/> 
                 </Routes>
