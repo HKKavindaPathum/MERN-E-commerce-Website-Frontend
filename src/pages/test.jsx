b52dfa2c407bc0cc90b5a0a91b8b47400b041db0
HKKavindaPathum <kavindapathum98@gmail.com>
2025-09-07 12:36:10 +0530

    remove test page

--- src/pages/test.jsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage(){
-
-    return(
-        <div className="w-full h-screen  flex justify-center items-center flex-col bg-red-900 md:bg-blue-900">
-                      
-        </div>
-    )
-}
